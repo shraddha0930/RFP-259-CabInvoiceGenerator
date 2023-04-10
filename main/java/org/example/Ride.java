2d76efe278dd0d9146ab4e81b8d0534a4a928a7b
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-10 14:04:12 +0530

    Added:code to the invoice generator should take multiple rides and calculate total for all

---
+++ main/java/org/example/Ride.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Ride {
+
+    double distance ;
+
+    double time ;
+
+    public Ride(double distance, double time) {
+        this.distance = distance;
+        this.time = time;
+    }
+}

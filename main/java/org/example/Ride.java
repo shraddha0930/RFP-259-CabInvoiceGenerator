04bef211ebdb4c51555646c3e85b8b1ec7266514
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-10 14:59:50 +0530

    Added:code to the cab agency now support 2 categories of rides

--- main/java/org/example/Ride.java
+++ main/java/org/example/Ride.java
@@ -1,13 +1,19 @@
 package org.example;
 
 public class Ride {
+    double distance;
+    double time;
 
-    double distance ;
-
-    double time ;
-
+    String type;
     public Ride(double distance, double time) {
         this.distance = distance;
         this.time = time;
     }
+
+    public Ride(double distance, double time, String type) {
+        this.distance = distance;
+        this.time = time;
+        this.type = type;
+    }
+
 }

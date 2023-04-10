be4c24b3196a7ac2dd7eb5a375a8e83997972b7f
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-10 14:33:58 +0530

    Added:code to the invoice service gets the list of rides from the riderepository and return the invoice

--- test/java/CabInvoiceTest.java
+++ test/java/CabInvoiceTest.java
@@ -1,6 +1,7 @@
 import org.example.CabInvoice;
 import org.example.Invoice;
 import org.example.Ride;
+import org.example.RideRepository;
 import org.junit.Test;
 import org.testng.Assert;
 
@@ -27,6 +28,7 @@ public class CabInvoiceTest {
         double aggregateTotalFare = cabInvoice.calculateTotalFare(rides);
         Assert.assertEquals(164.0,aggregateTotalFare);
     }
+
     @Test
     public void givenMultipleRides_ShouldReturnInvoice(){
         CabInvoice cabInvoice = new CabInvoice();
@@ -34,5 +36,20 @@ public class CabInvoiceTest {
         Invoice expectedInvoice = new Invoice(3,234.0,78.0);
         Invoice actualInvoice = cabInvoice.generateInvoice(rides);
         Assert.assertEquals(expectedInvoice,actualInvoice);
+    }
+
+    @Test
+    public void givenUserId_ShouldReturnInvoice(){
+        RideRepository rideRepository = new RideRepository();
+        Ride[] ride1 = {new Ride(10.0,4.0),new Ride(12.0,5.0),new Ride(0.1,1.0)};
+        Ride[] ride2 = {new Ride(12.0,3.0),new Ride(14.0,5.0),new Ride(0.1,3.0)};
+        rideRepository.addRides(1,ride1);
+        rideRepository.addRides(2,ride2);
+        Invoice expectedInvoice1 = new Invoice(3,234.0,78.0);
+        Invoice expectedInvoice2 = new Invoice(3,273.0,91.0);
+        Invoice actualInvoice1 = rideRepository.getInvoiceFromUserId(1);
+        Invoice actualInvoice2 = rideRepository.getInvoiceFromUserId(2);
+        Assert.assertEquals(expectedInvoice1,actualInvoice1);
+        Assert.assertEquals(expectedInvoice2,actualInvoice2);
     }
 }

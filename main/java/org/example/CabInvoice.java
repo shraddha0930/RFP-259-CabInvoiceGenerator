bf1f504a79d4085db6b0b370f45d11d01f5c16c5
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-10 14:03:56 +0530

    Added:code to the invoice generator should take multiple rides and calculate total for all

--- main/java/org/example/CabInvoice.java
+++ main/java/org/example/CabInvoice.java
@@ -4,12 +4,23 @@ public class CabInvoice {
     static final double COST_PER_KM = 10.0;
     static final double COST_PER_MIN = 1.0;
 
+    static final double MINIMUM_FARE = 5.0;
+
     public CabInvoice() {
     }
 
 
     public double calculateTotalFare(double distance, double time) {
         double totalFare = distance*COST_PER_KM + time*COST_PER_MIN;
-        return totalFare;
+        return totalFare<MINIMUM_FARE ? MINIMUM_FARE : totalFare;
+    }
+
+    public double calculateTotalFare(Ride[] rides) {
+        double aggregateTotalFare = 0.0;
+        for (Ride ride:rides) {
+            double totalFare = calculateTotalFare(ride.distance,ride.time);
+            aggregateTotalFare += totalFare;
+        }
+        return aggregateTotalFare;
     }
 }

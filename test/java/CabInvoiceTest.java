0602b9a29029db9b68637456b6936440684a0b67
shraddha0930 <shraddhaingole11@gmail.com>
2023-04-10 14:04:41 +0530

    Added:code to the invoice generator should take multiple rides and calculate total for all

--- test/java/CabInvoiceTest.java
+++ test/java/CabInvoiceTest.java
@@ -1,6 +1,7 @@
 import org.example.CabInvoice;
-import org.junit.Assert;
+import org.example.Ride;
 import org.junit.Test;
+import org.testng.Assert;
 
 public class CabInvoiceTest {
 
@@ -9,5 +10,20 @@ public class CabInvoiceTest {
         CabInvoice cabInvoice = new CabInvoice();
         double totalFare = cabInvoice.calculateTotalFare(10.0,4.0);
         Assert.assertEquals(104.0,totalFare);
+    }
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare(){
+        CabInvoice cabInvoice = new CabInvoice();
+        double totalFare = cabInvoice.calculateTotalFare(0.1,1.0);
+        Assert.assertEquals(5.0,totalFare);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnAggregateTotalFare(){
+        CabInvoice cabInvoice = new CabInvoice();
+        Ride[] rides = {new Ride(10.0,4.0),new Ride(5.0,5.0),new Ride(0.1,1.0)};
+        double aggregateTotalFare = cabInvoice.calculateTotalFare(rides);
+        Assert.assertEquals(164.0,aggregateTotalFare);
     }
 }
